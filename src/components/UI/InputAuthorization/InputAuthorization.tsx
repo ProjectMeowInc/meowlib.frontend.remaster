0fe0630af8acab62eedb12828efe3b432ef1885a
k3ssL <viqwer3@yandex.ru>
2023-11-11 01:07:38 +0500

    Fix Компоненты "Button" и "InputAuthorization"

--- src/components/UI/InputAuthorization/InputAuthorization.tsx
+++ src/components/UI/InputAuthorization/InputAuthorization.tsx
@@ -4,14 +4,16 @@ import classes from "./InputAuthorization.module.css";
 interface IInputAuthorizationProps {
     onChange: () => void;
     placeholder: string;
+    margin?:string;
 }
 
-const InputAuthorization: React.FC<IInputAuthorizationProps> = ({placeholder, onChange}) => {
+const InputAuthorization: React.FC<IInputAuthorizationProps> = ({placeholder, onChange, margin}) => {
     return (
             <input
                 placeholder={placeholder}
                 className={classes.inp_auth}
                 onChange={onChange}
+                style={{margin}}
             />
     );
 };
--- src/components/UI/button/Button.tsx
+++ src/components/UI/button/Button.tsx
@@ -3,12 +3,18 @@ import classes from "./Button.module.css";
 
 interface IButtonProps {
     children: string;
-    onClick?: () => void;
+    onClick: () => void;
+    margin?:string;
+    width?:string;
 }
 
-const Button: React.FC<IButtonProps> = ({children, onClick}) => {
+const Button: React.FC<IButtonProps> = ({children, onClick, margin, width}) => {
     return (
-        <button className={classes.btn} onClick={onClick}>
+        <button
+            className={classes.btn}
+            onClick={onClick}
+            style={{margin,width}}
+        >
             {children}
         </button>
     );
