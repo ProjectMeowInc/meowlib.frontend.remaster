8deec5452ae2c4addfdf47781d6564d2ff6f6b7f
k3ssL <viqwer3@yandex.ru>
2023-11-11 01:00:27 +0500

    Создание страницы авторизации "auth"

---
+++ src/pages/auth.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Authorization from "@/components/UI/authorization/Authorization";
+
+const Auth = () => {
+    return (
+        <>
+
+        </>
+    );
+};
+
+export default Auth;
