81c17f609ff21af3d95d4e6bcb458cf1e89e147f
lx1cx <lagunov.2019@list.ru>
2023-11-13 23:06:38 +0500

    Написан Result

---
+++ src/shared/services/Result/Result.ts
@@ -0,0 +1,53 @@
+import {IError} from "@/shared/services/interfaces/IError";
+import {IValidationError} from "@/shared/services/interfaces/IValidationError";
+
+export class Result<TType> {
+    private readonly value: TType | null
+    private readonly error: IError | IValidationError | null
+
+    constructor(value: TType | null, error: IError| IValidationError | null) {
+        this.value = value
+        this.error = error
+    }
+
+    unwrap(): TType {
+        if (!this.value) {
+            throw new Error("Значение не может быть пусто")
+        }
+
+        return this.value
+    }
+
+    hasError(): boolean {
+        return this.error !== null
+    }
+
+    getError(): IError | IValidationError {
+        if (!this.error) {
+            throw new Error("Ошибка не может быть пуста")
+        }
+
+        return this.error
+    }
+
+    isValidationError(): boolean {
+
+        if (!this.error) {
+            throw new Error("Ошибка не может быть пуста")
+        }
+
+        if ("validationError" in this.error) {
+            return true
+        }
+
+        return false
+    }
+
+    static withOk<TResult>(value: TResult): Result<TResult> {
+        return new Result<TResult>(value, null)
+    }
+
+    static withError<TResult>(error: IError | IValidationError): Result<TResult> {
+        return new Result<TResult>(null, error)
+    }
+}
