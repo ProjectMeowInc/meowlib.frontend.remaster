d0cb9b7ebefd9a71a32ea8103fd77de4b8c17153
k3ssL <viqwer3@yandex.ru>
2023-11-26 00:29:16 +0500

    Написание логики для компонента "RegistrationForm"

--- src/pages/AuthPage/UI/RegistrationForm/RegistrationForm.tsx
+++ src/pages/AuthPage/UI/RegistrationForm/RegistrationForm.tsx
@@ -2,22 +2,36 @@ import React from "react"
 import InputAuthorization from "@/shared/UI/InputAuthorization/InputAuthorization"
 import Button from "@/shared/UI/button/Button"
 import classes from "./RegistrationForm.module.css"
+import { useRegistration } from "@/entities/Auth/hooks/useRegistration"
 
 const RegistrationForm = () => {
+    const { formData, handleInputChange, handleSubmit } = useRegistration()
+
     return (
         <div className={classes.container__reg_form}>
             <h2>Регистрация через логин и пароль</h2>
             <hr className={classes.reg_form__hr_one} />
-            <form>
-                <InputAuthorization placeholder={"Введите логин"} styles={{ margin: "20px 42px" }} />
-
-                <InputAuthorization placeholder={"Введите пароль"} styles={{ margin: "0 42px" }} />
-
+            <form onSubmit={handleSubmit}>
+                <InputAuthorization
+                    type={"text"}
+                    name={"login"}
+                    value={formData.login}
+                    onChange={handleInputChange}
+                    placeholder={"Введите логин"}
+                    styles={{ margin: "20px 42px" }}
+                />
+                <InputAuthorization
+                    type={"password"}
+                    name={"password"}
+                    value={formData.password}
+                    onChange={handleInputChange}
+                    placeholder={"Введите пароль"}
+                    styles={{ margin: "0 42px" }}
+                />
                 <hr className={classes.reg_form__hr_two} />
                 <Button styles={{ margin: "30px 42px", width: "312px" }}>Зарегистрироваться</Button>
             </form>
         </div>
     )
 }
-
 export default RegistrationForm
