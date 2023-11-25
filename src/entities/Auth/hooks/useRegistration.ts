08645bd3fe8c84c1a8092dac662360e1f621e228
k3ssL <viqwer3@yandex.ru>
2023-11-26 00:28:14 +0500

    Создание хука 'useRegistration'

---
+++ src/entities/Auth/hooks/useRegistration.ts
@@ -0,0 +1,41 @@
+import React, { useState } from "react"
+import { ISignInRequest } from "@/entities/Auth/models/requests/SignInRequests"
+import { AuthService } from "@/entities/Auth/service/AuthService"
+import { AlertService } from "@/shared/services/AlertService"
+
+export const useRegistration = () => {
+    const [formData, setFormData] = useState<ISignInRequest>({
+        login: "",
+        password: "",
+    })
+
+    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = event.target
+        setFormData((prevFormData) => ({
+            ...prevFormData,
+            [name]: value,
+        }))
+    }
+
+    async function handleSubmit(event: React.ChangeEvent): Promise<void> {
+        event.preventDefault()
+        const result = await AuthService.registration(formData.login, formData.password)
+
+        console.log(formData)
+
+        setFormData({
+            login: "",
+            password: "",
+        })
+        if (result.hasError()) {
+            AlertService.errorMessage(result.getError().errorMessage)
+        }
+        return AlertService.successMessage("Вы успешно зарегистрировались")
+    }
+
+    return {
+        formData,
+        handleInputChange,
+        handleSubmit,
+    }
+}
