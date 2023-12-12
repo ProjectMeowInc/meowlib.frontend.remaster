a918af17dd5d82b7025a5966b24c8a27effd1d19
k3ssL <viqwer3@yandex.ru>
2023-12-13 00:01:22 +0500

    Добавлена документация

--- src/shared/UI/InputAuthorization/InputAuthorization.tsx
+++ src/shared/UI/InputAuthorization/InputAuthorization.tsx
@@ -2,7 +2,17 @@ import React from "react"
 import classes from "./InputAuthorization.module.css"
 import { IOnChangeEvent } from "@/shared/models/events/IOnChangeEvent"
 
-// TODO: добавить документацию
+/**
+ * Описание интерфейса пропсов инпута авторизации
+ * @param onChange принимает в себя интерфейс в котором описаны данные в веденные в инпут и привязка поля ввода для отправки на сервер
+ * при срабатывании евента, а так же саму функцию
+ * @param placeholder строка которая будет отображаться в инпуте
+ * @param styles объект, принимает в себя стили используемые компонентом
+ * @param value хранит в себе данные введенные пользователем
+ * @param type тип самого инпута
+ * @param name используется для "привязки" данных к отправляемому полю на сервер, например данные с инпута с полем name={'login'},
+ * будут отправлены на сервер в виде login='введенные в инпут данные'
+ * */
 interface IInputAuthorizationProps {
     onChange?: (event: IOnChangeEvent) => void
     placeholder: string
@@ -11,8 +21,8 @@ interface IInputAuthorizationProps {
     }
     value?: string
     type?: string
-    // TODO: особенно для этого
     name: string
+
 }
 
 const InputAuthorization: React.FC<IInputAuthorizationProps> = ({
