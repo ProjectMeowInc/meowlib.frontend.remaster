4bb5099f81ca6990dbd1c9f9b66f1c6dcc608a08
k3ssL <viqwer3@yandex.ru>
2023-12-13 23:22:58 +0500

    Изменены модели DTO

--- src/entities/Auth/models/dto/LogInDto.ts
+++ src/entities/Auth/models/dto/LogInDto.ts
@@ -1,28 +1,4 @@
-import { ILoginResponse } from "@/entities/Auth/models/response/LogInResponse"
-import { ILogInRequest } from "@/entities/Auth/models/requests/LogInRequest"
-
-export interface ILogInRequestInDto {
-    login: string
-    password: string
-    isLongSession: boolean
-}
-
-export interface ILogInResponseInDto {
+export interface ILogInDto {
     accessToken: string
     refreshToken: string
-}
-
-export function mapRequestLogInToDTO(request: ILogInRequest): ILogInRequestInDto {
-    return {
-        login: request.login,
-        password: request.password,
-        isLongSession: request.isLongSession,
-    }
-}
-
-export function mapResponseLogInToDTO(response: ILoginResponse): ILogInResponseInDto {
-    return {
-        accessToken: response.accessToken,
-        refreshToken: response.refreshToken,
-    }
 }
--- src/entities/Auth/models/dto/UpdateAuthDto.ts
+++ src/entities/Auth/models/dto/UpdateAuthDto.ts
@@ -1,24 +1,4 @@
-import { IUpdateAuthResponse } from "@/entities/Auth/models/response/UpdateAuthResponse"
-import { IUpdateAuthRequest } from "@/entities/Auth/models/requests/UpdateAuthRequest"
-
-export interface IUpdateAuthRequestInDto {
-    refreshToken: string
-}
-
-export interface IUpdateAuthResponseInDto {
+export interface IUpdateAuthDto {
     refreshToken: string
     accessToken: string
-}
-
-export function mapRequestUpdateAuthToDto(request: IUpdateAuthRequest): IUpdateAuthRequestInDto {
-    return {
-        refreshToken: request.refreshToken,
-    }
-}
-
-export function mapResponseUpdateAuthToDto(response: IUpdateAuthResponse): IUpdateAuthResponseInDto {
-    return {
-        refreshToken: response.refreshToken,
-        accessToken: response.accessToken,
-    }
 }
