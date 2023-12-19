f2509eef7ca3f4e7b3223889a7815e09772f28eb
lx1cx <lagunov.2019@list.ru>
2023-12-19 23:06:43 +0500

    Сделана главная страница админки

---
+++ src/pages/admin/MainAdminPage/MainAdminPage.tsx
@@ -0,0 +1,19 @@
+import MainAdminPageListItem from "@/pages/admin/MainAdminPage/UI/MainAdminPageListItem/MainAdminPageListItem"
+import classes from "./mainAdminPage.module.css"
+
+const MainAdminPage = () => {
+    return (
+        <div className={classes.wrapper}>
+            <div className={classes.panel}>
+                <h1>Админ панель</h1>
+                <div className={classes.items}>
+                    <MainAdminPageListItem href={"/admin/tags"}>
+                        Теги
+                    </MainAdminPageListItem>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default MainAdminPage
