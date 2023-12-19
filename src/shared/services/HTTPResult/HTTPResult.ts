69a2153562ed08a65a5feee78d61906d205ecbed
k3ssL <viqwer3@yandex.ru>
2023-12-19 16:43:14 +0500

    Добавлен контроль версий

--- src/shared/services/HTTPResult/HTTPResult.ts
+++ src/shared/services/HTTPResult/HTTPResult.ts
@@ -7,7 +7,7 @@ import { IValidationErrorResponse } from "@/shared/models/IValidationErrorRespon
 import { LogService } from "@/shared/services/LogService"
 
 type MethodsType = "GET" | "POST" | "PUT" | "DELETE"
-const BASE_URL: string = "http://127.0.0.1:5000/api"
+const BASE_URL: string = "http://127.0.0.1:5000/api/v1"
 
 export class HTTPResult<TContent> {
     private method: MethodsType = "GET"
@@ -121,9 +121,5 @@ export class HTTPResult<TContent> {
     withDeleteMethod() {
         this.method = "DELETE"
         return this
-    }
-
-    public static create<TContent>(): HTTPResult<TContent> {
-        return new HTTPResult()
     }
 }
