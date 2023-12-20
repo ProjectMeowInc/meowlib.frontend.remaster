78c407d5aef9a98e600e96faa63037e83f8fa436
lx1cx <lagunov.2019@list.ru>
2023-12-20 20:49:23 +0500

    начата страница для тегов

--- src/pages/admin/tags/MainTagPage/MainTagPage.tsx
+++ src/pages/admin/tags/MainTagPage/MainTagPage.tsx
@@ -1,4 +1,15 @@
-const MainTagPage = () => {
+import { TagService } from "@/entities/Tag/services/TagService"
+
+const MainTagPage = async () => {
+
+    const getTagsResult = await TagService.getAllAsync()
+
+    if (getTagsResult.hasError()) {
+        return <div>{getTagsResult.getError().errorMessage}</div>
+    }
+
+    const tags = getTagsResult.unwrap()
+
     return (
         <div>
 
