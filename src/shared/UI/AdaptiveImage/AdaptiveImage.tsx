713e525d451cbf4024f1964e9dce9ee8b9043a7b
blacksoulhub <mr.asaglaev@mail.ru>
2023-12-21 12:02:15 +0400

    add adaptive image component

---
+++ src/shared/UI/AdaptiveImage/AdaptiveImage.tsx
@@ -0,0 +1,20 @@
+import { FC } from "react"
+
+interface IAdaptiveImageProps {
+    style?: {
+        width?: string
+        height?: string
+    }
+    className?: string
+    url: string
+}
+
+const AdaptiveImage: FC<IAdaptiveImageProps> = ({url, style, className}) => {
+    return (
+        <div className={className} style={{...style, backgroundImage: `url(${url})`, backgroundSize: "cover"}}>
+
+        </div>
+    )
+}
+
+export default AdaptiveImage
