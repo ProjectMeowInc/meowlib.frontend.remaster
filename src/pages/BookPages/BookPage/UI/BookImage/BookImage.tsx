3c99167354e8779cd12ad6ce92f80766f522933c
BlackSoulHub <mr.asaglaev@mail.ru>
2023-12-21 22:28:58 +0500

    create book image component

--- src/pages/BookPages/BookPage/UI/BookImage/BookImage.tsx
+++ src/pages/BookPages/BookPage/UI/BookImage/BookImage.tsx
@@ -1,5 +1,6 @@
 import { FC } from "react"
 import classes from "./bookImage.module.css"
+import Button from "@/shared/UI/button/Button";
 
 interface IBookImageProps {
     image: string
@@ -12,9 +13,17 @@ const BookImage: FC<IBookImageProps> = ({ image }) => {
                 className={classes.background_image}
                 style={{backgroundImage: `url(${image})`, backgroundSize: "cover"}}>
                 <div
+                    className={classes.main_image}
                     style={{backgroundImage: `url(${image})`, backgroundSize: "cover"}}>
-
                 </div>
+            </div>
+            <div className={classes.buttons}>
+                <Button className={classes.button}>
+                    Продолжить читать
+                </Button>
+                <Button className={classes.button}>
+                    Добавить в список
+                </Button>
             </div>
         </div>
     )
