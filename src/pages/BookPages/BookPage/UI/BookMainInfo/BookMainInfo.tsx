b5ac5b548f4884f3a885c5baac1c2e90a0597a82
BlackSoulHub <mr.asaglaev@mail.ru>
2023-12-21 23:01:28 +0500

    create book main info component

--- src/pages/BookPages/BookPage/UI/BookMainInfo/BookMainInfo.tsx
+++ src/pages/BookPages/BookPage/UI/BookMainInfo/BookMainInfo.tsx
@@ -1,5 +1,42 @@
-const BookMainInfo = () => {
-    return <div></div>
+import {FC} from "react";
+import classes from "./bookMainInfo.module.css"
+import TagItem from "@/pages/BookPages/BookPage/UI/TagItem/TagItem";
+import CommentList from "@/pages/BookPages/BookPage/UI/CommentList/CommentList";
+
+interface IBookMainInfoProps {
+    id: number
+    name: string
+    description: string
+    tags: {
+        id: number
+        name: string
+    }[]
+}
+
+const BookMainInfo: FC<IBookMainInfoProps> = ({id, name, description, tags}) => {
+    return (
+        <div>
+            <div className={classes.name}>
+                {name}
+            </div>
+            <div>
+                {description}
+            </div>
+            <div>
+                {
+                    tags.map(tag => (
+                        <TagItem {...tag} />
+                    ))
+                }
+            </div>
+            <div>
+                <div>
+                    Комментарии:
+                </div>
+                <CommentList bookId={id}/>
+            </div>
+        </div>
+    )
 }
 
 export default BookMainInfo
