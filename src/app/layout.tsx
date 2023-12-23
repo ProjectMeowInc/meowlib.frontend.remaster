d48f38b8a110f11df1b5319054e6374d8d1c9e54
lx1cx <lagunov.2019@list.ru>
2023-12-23 23:18:52 +0500

    Сделана сео оптимизация

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,26 @@
 import React from "react";
 import classes from "./root.module.css"
+import RootProvider from "@/shared/providers/RootProvider"
 
 export const metadata = {
   title: 'Meow Lib',
+  description: "Лучший сайт для чтения ранобэ",
+  keywords: [
+    "ранобэ",
+    "читать",
+    "читать ранобэ",
+    "тома ранобэ",
+    "ранобэ +на русском",
+    "бесплатное ранобэ",
+    "ранобэ читать онлайн",
+    "читать ранобэ бесплатно",
+    "ранобэ читать +на русском",
+    "новелла +это",
+    "новелла",
+  ]
 }
+
+
 
 export default function RootLayout({
   children,
@@ -12,7 +29,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={classes.app_body}>{children}</body>
+      <body className={classes.app_body}>
+        <RootProvider>
+          {children}
+        </RootProvider>
+      </body>
     </html>
   )
 }
