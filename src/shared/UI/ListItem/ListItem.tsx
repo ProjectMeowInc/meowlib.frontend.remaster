b5b3c9231b10905b02406bce9a1cb7c55c8acb74
lx1cx <lagunov.2019@list.ru>
2023-12-23 23:12:17 +0500

    Рефакторинг

--- src/shared/UI/ListItem/ListItem.tsx
+++ src/shared/UI/ListItem/ListItem.tsx
@@ -1,17 +1,30 @@
+"use client"
+
 import Link from "next/link"
 import { FC } from "react"
 import classes from "./listItem.module.css"
+import { usePathname } from "next/navigation"
 
 interface IListItemProps {
+    id: number
     text: string
     href: string
+    onDelete: (id: number) => Promise<void>
 }
 
-const ListItem: FC<IListItemProps> = ({text, href}) => {
+const ListItem: FC<IListItemProps> = ({id, text, href, onDelete}) => {
+
+    const pathname = usePathname()
+
     return (
-        <Link className={classes.link} href={href}>
-            {text}
-        </Link>
+        <div className={classes.link}>
+            <div className={classes.link__wrapper}>
+                <Link className={classes.link_text} href={pathname + href}>
+                    {text}
+                </Link>
+                <p className={classes.delete_btn} onClick={async () => await onDelete(id)}>Удалить</p>
+            </div>
+        </div>
     )
 }
 
