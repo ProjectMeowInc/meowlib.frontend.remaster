ba3bcca8e95c4276c57a444cf771e1e63dab1edc
lx1cx <lagunov.2019@list.ru>
2023-12-23 23:17:08 +0500

    Рефакторинг

--- src/pages/admin/tags/MainTagPage/MainTagPage.tsx
+++ src/pages/admin/tags/MainTagPage/MainTagPage.tsx
@@ -1,6 +1,7 @@
 import { TagService } from "@/entities/Tag/services/TagService"
 import AdminCreateButton from "@/pages/admin/UI/AdminCreateButton/AdminCreateButton"
 import classes from "./mainTagPage.module.css"
+import TagList from "@/pages/admin/tags/MainTagPage/UI/TagList"
 
 const MainTagPage = async () => {
 
@@ -15,6 +16,7 @@ const MainTagPage = async () => {
     return (
         <div className={classes.wrapper}>
             <AdminCreateButton text={"Создать тег"} href={"/new"} />
+            <TagList tags={tags}/>
         </div>
     )
 }
