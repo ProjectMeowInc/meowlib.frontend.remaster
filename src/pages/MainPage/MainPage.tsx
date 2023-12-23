1f7a1ad0032bfce92a6a60a32461786b0b308020
lx1cx <lagunov.2019@list.ru>
2023-12-23 23:13:55 +0500

    Рефакторинг

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,9 +1,10 @@
 import React from "react"
+import BookPreview from "@/pages/MainPage/UI/BookPreview/BookPreview"
 import classes from "@/pages/MainPage/MainPage.module.css"
 import Menu from "@/pages/MainPage/UI/Menu/Menu"
 import Header from "@/shared/UI/Header/Header"
 import SearchInput from "@/pages/MainPage/UI/SearchInput/SearchInput"
-import MainPageBooks from "@/pages/MainPage/UI/MainPageBooks/MainPageBooks";
+import MainPageBooks from "@/pages/MainPage/UI/MainPageBooks/MainPageBooks"
 
 const MainPage = () => {
 
@@ -13,6 +14,7 @@ const MainPage = () => {
             <Menu />
             <SearchInput placeholder={"Enter search terms"} />
             <MainPageBooks/>
+            <BookPreview />
         </div>
     )
 }
