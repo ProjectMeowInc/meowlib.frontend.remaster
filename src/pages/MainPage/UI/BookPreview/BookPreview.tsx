79b564a946ffed4e632933727039d80dceaf47f6
k3ssL <viqwer3@yandex.ru>
2023-12-25 00:47:13 +0500

    Компонент может принимать данные из массива

--- src/pages/MainPage/UI/BookPreview/BookPreview.tsx
+++ src/pages/MainPage/UI/BookPreview/BookPreview.tsx
@@ -1,12 +1,14 @@
 import React from "react"
 import classes from "./BookPreview.module.css"
+import {BookGetResponseDTO} from "@/entities/Book/model/dto/BookGetResponseDTO";
 
-const BookPreview = () => {
+
+const BookPreview:React.FC<BookGetResponseDTO> = ({imageName, name, author}) => {
     return (
         <div className={classes.container}>
-            <img src={'/img/4.png'} alt={''}/>
-            <h4>Добро пожаловать в класс...</h4>
-            <p>Автор</p>
+            <img src={`${imageName}`}/>
+            <h4>{name}</h4>
+            <p>{author}</p>
         </div>
     )
 }
