3c17235d88f616731cafa1251ef07dd00fd82f79
lx1cx <lagunov.2019@list.ru>
2023-12-24 13:04:01 +0500

    warnings

--- src/pages/BookPages/BookPage/UI/CommentList/useCommentList.ts
+++ src/pages/BookPages/BookPage/UI/CommentList/useCommentList.ts
@@ -1,7 +1,6 @@
 import {useEffect, useState} from "react";
 import {ICommentDto} from "@/entities/BookComments/models/dto/ICommentDto";
 import {LogService} from "@/shared/services/LogService";
-import {UserRoleEnum} from "@/entities/User/User";
 
 export const useCommentList = () => {
     const [comments, setComments] = useState<ICommentDto[] | null>(null)
