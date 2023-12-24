0b64f37ddf22a06de68391f59538e1c95dbd080d
k3ssL <viqwer3@yandex.ru>
2023-12-25 00:49:05 +0500

    Div изменен на Link(Решение конфликтов)

--- src/pages/MainPage/UI/Menu/Menu.tsx
+++ src/pages/MainPage/UI/Menu/Menu.tsx
@@ -6,17 +6,17 @@ const Menu = () => {
     return (
         <aside className={classes.container}>
             <div className={classes.top_part}>
-                <MenuItem img={"/img/icon.png"} text={"Главная"} />
+                <MenuItem img={"/img/icon.png"} text={"Главная"} href={''} />
 
-                <MenuItem img={"/img/6.png"} text={"Избранное"} />
+                <MenuItem img={"/img/6.png"} text={"Избранное"} href={''} />
 
-                <MenuItem img={"/img/7.png"} text={"Админ панель"} />
+                <MenuItem img={"/img/7.png"} text={"Админ панель"} href={''} />
             </div>
 
             <div className={classes.bottom_part}>
-                <MenuItem img={"/img/8.png"} text={"Настройки"} />
+                <MenuItem img={"/img/8.png"} text={"Настройки"} href={''} />
 
-                <MenuItem img={"/img/9.png"} text={"Выйти"} />
+                <MenuItem img={"/img/9.png"} text={"Выйти"} href={''} />
             </div>
         </aside>
     )
--- src/pages/MainPage/UI/MenuItem/MenuItem.tsx
+++ src/pages/MainPage/UI/MenuItem/MenuItem.tsx
@@ -2,14 +2,16 @@ import React from "react"
 import classes from "./MenuItem.module.css"
 import Link from "next/link";
 
+
 interface IMenuItemProps {
     img: string
     text: string
+    href: string
 }
 
-const MenuItem: React.FC<IMenuItemProps> = ({ img, text }) => {
+const MenuItem: React.FC<IMenuItemProps> = ({ img, text, href }) => {
     return (
-        <Link className={classes.container} href={''}>
+        <Link className={classes.container} href={href}>
             <img src={img} alt={""} />
             <p>{text}</p>
             <img src={"/img/5.png"} alt={''} />
