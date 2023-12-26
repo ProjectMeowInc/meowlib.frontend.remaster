840e01afc1a038ce3f6f1206b3f430c5836c6fcd
k3ssL <viqwer3@yandex.ru>
2023-12-26 22:28:50 +0500

    Создан компонент layout

---
+++ src/shared/Layout/Layout.tsx
@@ -0,0 +1,16 @@
+import React, {PropsWithChildren} from 'react';
+import Header from "@/shared/UI/Header/Header";
+import Menu from "@/pages/MainPage/UI/Menu/Menu";
+import classes from './Layout.module.css'
+
+const Layout:React.FC<PropsWithChildren> = ({children}) => {
+    return (
+        <div className={classes.container}>
+            <Header/>
+            <Menu/>
+            {children}
+        </div>
+    );
+};
+
+export default Layout;
