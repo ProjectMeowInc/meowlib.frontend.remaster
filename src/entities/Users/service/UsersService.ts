d2710d0903e4f83a3e8c0e8c35e5f8f6959f147f
k3ssL <viqwer3@yandex.ru>
2023-12-27 23:32:55 +0500

    написан сервис

---
+++ src/entities/Users/service/UsersService.ts
@@ -0,0 +1,22 @@
+import {Result} from "@/shared/services/Result/Result";
+import {IUserDTO} from "@/entities/Users/models/dto/IUserDTO";
+import {UsersApi} from "@/entities/Users/api/UsersApi";
+import {IUpdateUserByIdRequest} from "@/entities/Users/models/requests/IUpdateUserByIdRequest";
+
+export class UsersService {
+    static async getAllUsers(): Promise<Result<IUserDTO[]>> {
+        const result = await UsersApi.getAllUsers()
+
+            if (result.hasError()) {
+                return Result.withError(result.getError())
+            }
+
+        return Result.withOk(result.unwrap().items)
+
+    }
+
+    static async updateUserById(id: number, requestData: IUpdateUserByIdRequest): Promise<Result<IUserDTO>> {
+        return UsersApi.updateUserById(id, requestData)
+    }
+
+}
