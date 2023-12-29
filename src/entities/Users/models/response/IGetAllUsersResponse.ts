87f84ef90092273cdb92fa81736e642f2fee3e37
k3ssL <viqwer3@yandex.ru>
2023-12-29 23:08:46 +0500

    изменен интерфейс

--- src/entities/Users/models/response/IGetAllUsersResponse.ts
+++ src/entities/Users/models/response/IGetAllUsersResponse.ts
@@ -1,9 +1,9 @@
-import {User} from "@/entities/User/User";
+import { UserRoleEnum } from "@/entities/User/User";
 
 export interface IGetAllUsersResponse {
     items: {
         id: number
         login: string
-        role: User
+        role: UserRoleEnum
     } []
 }
