9e1c55f12b63c3042cfa4af24ec7f20eeff912db
k3ssL <viqwer3@yandex.ru>
2023-12-30 02:02:34 +0500

    создан элемент информации пользователя

---
+++ src/pages/admin/users/UI/UserItem/UserItem.tsx
@@ -0,0 +1,27 @@
+import React, {FC} from 'react';
+import { UserRoleEnum } from "@/entities/User/User";
+import classes from './UserItem.module.css'
+import Link from 'next/link';
+
+interface IUserItemProps {
+    id: number
+    login: string
+    role: UserRoleEnum
+    href: string
+    onUpdate?: (id: number) => Promise<void>
+}
+
+const UserItem: FC<IUserItemProps> = ({id, login, role}) => {
+
+    return (
+        <div className={classes.container}>
+            <div className={classes.content}>
+                <p className={classes.login}>{login}</p>
+                <p className={classes.role}>{role}</p>
+                <Link href={`${id}/edit`} className={classes.link_text}> Изменить </Link>
+            </div>
+        </div>
+    );
+};
+
+export default UserItem;
