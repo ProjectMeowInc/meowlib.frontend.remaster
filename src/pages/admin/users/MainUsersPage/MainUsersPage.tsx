041189365c982c205ad47ce110db4f0fdaffeb89
lx1cx <lagunov.2019@listy.ru>
2023-12-31 00:02:16 +0500

    пробел

--- src/pages/admin/users/MainUsersPage/MainUsersPage.tsx
+++ src/pages/admin/users/MainUsersPage/MainUsersPage.tsx
@@ -1,10 +1,12 @@
+"use client"
+
 import React, {useEffect, useState} from 'react';
 import classes from './MainUsersPage.module.css'
 import { UsersService } from "@/entities/Users/service/UsersService";
 import UserItem from "@/pages/admin/users/UI/UserItem/UserItem";
 import {IUserDTO} from "@/entities/Users/models/dto/IUserDTO";
 
-const  MainUsersPage =  () => {
+const  MainUsersPage = () => {
 
     const [userList, setUserList] = useState<IUserDTO[] | null>(null)
 
