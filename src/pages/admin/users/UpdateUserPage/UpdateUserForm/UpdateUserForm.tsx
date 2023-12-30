c383ea6fc6c19f45be0e89b608de888cc4dbbd0c
k3ssL <viqwer3@yandex.ru>
2023-12-31 00:45:36 +0500

    написаны стили для формы обновления пользователя

--- src/pages/admin/users/UpdateUserPage/UpdateUserForm/UpdateUserForm.tsx
+++ src/pages/admin/users/UpdateUserPage/UpdateUserForm/UpdateUserForm.tsx
@@ -5,6 +5,7 @@ import Input from "@/shared/UI/Input/Input";
 import {useUpdateUserForm} from "@/pages/admin/users/UpdateUserPage/useUpdateUserForm";
 import Button from "@/shared/UI/button/Button";
 import {IUserDTO} from "@/entities/Users/models/dto/IUserDTO";
+import classes from './UpdateUserForm.module.css'
 
 interface IUpdateUserProps {
     user: IUserDTO
@@ -14,11 +15,15 @@ const UpdateUserForm: FC<IUpdateUserProps> = ({ user }) => {
     const { ChangeHandler, SubmitHandler } = useUpdateUserForm(user.id)
 
     return (
+      <div className={classes.container}>
         <form onSubmit={SubmitHandler}>
-            <Input name={'login'} placeholder={'dasd'} onChange={ChangeHandler}/>
-            <Input name={'password'} placeholder={'asdasd'} onChange={ChangeHandler}/>
-            <Button> Отправить </Button>
+            <h1>Обновление пользователя</h1>
+            <Input name={'login'} placeholder={'Введите логин'} onChange={ChangeHandler} style={{margin: 10}} />
+            <Input name={'password'} placeholder={'Введите пароль'} type={"password"} onChange={ChangeHandler} style={{margin: 10}} />
+            <Input name={'role'} placeholder={'Введите роль'} onChange={ChangeHandler} style={{margin: 10}}/>
+            <Button styles={{width: '103%', margin: '10px'}}> Отправить </Button>
         </form>
+      </div>
     );
 };
 
