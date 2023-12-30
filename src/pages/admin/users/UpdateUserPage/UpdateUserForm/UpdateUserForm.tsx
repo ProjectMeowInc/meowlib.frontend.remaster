d9e3246f814149cd957b95badabeb3208a7706dc
lx1cx <lagunov.2019@listy.ru>
2023-12-31 00:03:32 +0500

    Сделан компонент клиентским

--- src/pages/admin/users/UpdateUserPage/UpdateUserForm/UpdateUserForm.tsx
+++ src/pages/admin/users/UpdateUserPage/UpdateUserForm/UpdateUserForm.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import React, {FC} from 'react';
 import Input from "@/shared/UI/Input/Input";
 import {useUpdateUserForm} from "@/pages/admin/users/UpdateUserPage/useUpdateUserForm";
