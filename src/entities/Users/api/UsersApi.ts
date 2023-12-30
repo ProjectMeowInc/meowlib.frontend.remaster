24e5094615106895e8cc079d9547cfd9f158a6dd
k3ssL <viqwer3@yandex.ru>
2023-12-30 13:30:41 +0500

    добавлен метод получения пользователя по id

--- src/entities/Users/api/UsersApi.ts
+++ src/entities/Users/api/UsersApi.ts
@@ -3,11 +3,25 @@ import {IGetAllUsersResponse} from "@/entities/Users/models/response/IGetAllUser
 import {HTTPResult} from "@/shared/services/HTTPResult/HTTPResult";
 import {IUpdateUserByIdRequest} from "@/entities/Users/models/requests/IUpdateUserByIdRequest";
 import {IUpdateUserByIdResponse} from "@/entities/Users/models/response/IUpdateUserByIdResponse";
+import {IGetUserByIdResponse} from "@/entities/Users/models/response/IGetUserByIdResponse";
 
 export class UsersApi {
     static async getAllUsers(): Promise<Result<IGetAllUsersResponse>> {
         const result = await new HTTPResult<IGetAllUsersResponse>()
             .withUrl('/v1/users')
+            .withGetMethod()
+            .sendAsync()
+
+        if (result.hasError()) {
+            return Result.withError(result.getError())
+        }
+
+        return Result.withOk(result.unwrap())
+    }
+
+    static async getUserById(id: number): Promise<Result<IGetUserByIdResponse>> {
+        const result = await new HTTPResult<IGetUserByIdResponse>()
+            .withUrl(`/v1/users/${id}`)
             .withGetMethod()
             .sendAsync()
 
@@ -32,5 +46,6 @@ export class UsersApi {
 
         return Result.withOk(result.unwrap())
     }
+
 
 }
