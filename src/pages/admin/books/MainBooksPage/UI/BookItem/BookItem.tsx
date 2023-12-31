b832a0077b7e5005e67e966b5796014e40f02a80
k3ssL <viqwer3@yandex.ru>
2023-12-31 23:45:51 +0500

    создан компонент элемента книги

--- src/pages/admin/books/MainBooksPage/UI/BookItem/BookItem.tsx
+++ src/pages/admin/books/MainBooksPage/UI/BookItem/BookItem.tsx
@@ -1,22 +1,33 @@
 import React, {FC} from 'react';
 import classes from './BookItem.module.css'
+import Link from "next/link";
+import {usePathname} from "next/navigation";
+import BookName from "@/pages/BookPages/BookPage/UI/BookName/BookName";
+import BookPreview from "@/pages/MainPage/UI/BookPreview/BookPreview";
 
 interface IBookItemProps {
     id: number
     name: string
     description: string
     imageUrl: string
-    author?: string
+    author: any
+    onDelete: (id: number) => Promise<void>
 }
 
-const BookItem: FC<IBookItemProps> = ({id, name, description, imageUrl , author}) => {
+const BookItem: FC<IBookItemProps> = ({id, name, description, imageUrl, author, onDelete}) => {
+
+
+    const pathname = usePathname()
+
     return (
         <div className={classes.container}>
-            <p>{id}</p>
-            <p>{name}</p>
+            <BookPreview id={id} image={imageUrl} name={name} author={author}/>
+            <h4>Описание:</h4>
             <p>{description}</p>
-            <p>{imageUrl}</p>
-            <p>{}</p>
+           <div className={classes.btns}>
+                <Link href={`${pathname}/${id}`} className={classes.link_text}>Изменить</Link>
+                <p onClick={async ()=> await onDelete(id)} className={classes.delete_btn}>Удалить</p>
+           </div>
         </div>
     );
 };
