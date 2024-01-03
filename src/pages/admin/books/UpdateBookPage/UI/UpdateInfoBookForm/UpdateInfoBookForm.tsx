a5528c8ffbdd39c1b179642fe4527ca25e0c3f31
lx1cx <lagunov.2019@listy.ru>
2024-01-04 00:35:47 +0500

    Рефакторинг

--- src/pages/admin/books/UpdateBookPage/UI/UpdateInfoBookForm/UpdateInfoBookForm.tsx
+++ src/pages/admin/books/UpdateBookPage/UI/UpdateInfoBookForm/UpdateInfoBookForm.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import React, {FC} from 'react';
 import Input from "@/shared/UI/Input/Input";
 import Button from "@/shared/UI/button/Button";
--- src/pages/admin/books/UpdateBookPage/UpdateBookPage.tsx
+++ src/pages/admin/books/UpdateBookPage/UpdateBookPage.tsx
@@ -11,23 +11,16 @@ interface IUpdateBookPageProps {
 }
 
 const UpdateBookPage: FC<IUpdateBookPageProps> = async ({ params: {bookId} }) => {
-    const getBookResult = await BookService.getBookById(bookId)
-
-    if (getBookResult.hasError()) {
-        return <div>{getBookResult.getError().errorMessage}</div>
-    }
-
-    const book = getBookResult.unwrap()
 
     return (
         <div className={classes.container}>
             <h1>Обновление книги</h1>
-            <MainAdminPageListItem href={`${book.id}/info`}>Информация</MainAdminPageListItem>
-            <MainAdminPageListItem href={`${book.id}/pic`}>Изображение</MainAdminPageListItem>
-            <MainAdminPageListItem href={`${book.id}/people`}>Люди</MainAdminPageListItem>
-            <MainAdminPageListItem href={`${book.id}/tags`}>Теги</MainAdminPageListItem>
-            <MainAdminPageListItem href={`${book.id}/trans`}>Переводы</MainAdminPageListItem>
-            <UpdateInfoBookForm book={book} styles={{display: 'none'}}/>
+            <MainAdminPageListItem href={`${bookId}/info`}>Информация</MainAdminPageListItem>
+            <MainAdminPageListItem href={`${bookId}/pic`}>Изображение</MainAdminPageListItem>
+            <MainAdminPageListItem href={`${bookId}/people`}>Люди</MainAdminPageListItem>
+            <MainAdminPageListItem href={`${bookId}/tags`}>Теги</MainAdminPageListItem>
+            <MainAdminPageListItem href={`${bookId}/trans`}>Переводы</MainAdminPageListItem>
+            {/*<UpdateInfoBookForm book={book} styles={{display: 'none'}}/>*/}
         </div>
     );
 };
