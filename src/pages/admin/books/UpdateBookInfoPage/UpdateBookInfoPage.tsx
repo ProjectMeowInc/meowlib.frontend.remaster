bed2e1eaea9ce67903c836129b00d5850daea6c3
lx1cx <lagunov.2019@listy.ru>
2024-01-04 00:35:37 +0500

    Вынесено в отдельную страницу

---
+++ src/pages/admin/books/UpdateBookInfoPage/UpdateBookInfoPage.tsx
@@ -0,0 +1,28 @@
+import { BookService } from "@/entities/Book/service/BookService"
+import React, { FC } from "react"
+import UpdateInfoBookForm from "@/pages/admin/books/UpdateBookPage/UI/UpdateInfoBookForm/UpdateInfoBookForm"
+
+interface IUpdateBookInfoPageProps {
+    params: {
+        bookId: number
+    }
+}
+
+const UpdateBookInfoPage: FC<IUpdateBookInfoPageProps> = async ({params: {bookId}}) => {
+
+    const getBookResult = await BookService.getBookById(bookId)
+
+    if (getBookResult.hasError()) {
+        return <div>{getBookResult.getError().errorMessage}</div>
+    }
+
+    const book = getBookResult.unwrap()
+
+    return (
+        <div>
+            <UpdateInfoBookForm book={book}/>
+        </div>
+    )
+}
+
+export default UpdateBookInfoPage
