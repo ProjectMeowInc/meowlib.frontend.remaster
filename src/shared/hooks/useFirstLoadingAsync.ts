1f45670fcf94eafcc792fd9a10c749d89592d357
blacksoulhub <mr.asaglaev@mail.ru>
2024-01-04 07:57:55 +0500

    fix warning

--- src/shared/hooks/useFirstLoadingAsync.ts
+++ src/shared/hooks/useFirstLoadingAsync.ts
@@ -3,5 +3,6 @@ import { useEffect } from "react"
 export const useFirstLoadingAsync = (callback: () => Promise<void>) => {
     useEffect(() => {
         callback.call(null).finally(() => {})
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
 }
