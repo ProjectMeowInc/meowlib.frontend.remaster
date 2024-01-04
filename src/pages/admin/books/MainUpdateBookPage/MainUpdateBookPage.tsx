cb9dd572f2d579dd9b31e3597641180b56241934
k3ssL <viqwer3@yandex.ru>
2024-01-04 23:19:28 +0500

    рефакторинг

--- src/pages/admin/books/MainUpdateBookPage/MainUpdateBookPage.tsx
+++ src/pages/admin/books/MainUpdateBookPage/MainUpdateBookPage.tsx
@@ -1,8 +1,6 @@
 import React, {FC} from 'react';
-import {BookService} from "@/entities/Book/service/BookService";
 import MainAdminPageListItem from "@/pages/admin/MainAdminPage/UI/MainAdminPageListItem/MainAdminPageListItem";
-import classes from './UpdateBookPage.module.css'
-import UpdateInfoBookForm from "@/pages/admin/books/UpdateBookPage/UI/UpdateInfoBookForm/UpdateInfoBookForm";
+import classes from './MainUpdateBookPage.module.css'
 
 interface IUpdateBookPageProps {
    params: {
@@ -10,19 +8,18 @@ interface IUpdateBookPageProps {
    }
 }
 
-const UpdateBookPage: FC<IUpdateBookPageProps> = async ({ params: {bookId} }) => {
+const MainUpdateBookPage: FC<IUpdateBookPageProps> = async ({ params: {bookId} }) => {
 
     return (
         <div className={classes.container}>
             <h1>Обновление книги</h1>
             <MainAdminPageListItem href={`${bookId}/info`}>Информация</MainAdminPageListItem>
-            <MainAdminPageListItem href={`${bookId}/pic`}>Изображение</MainAdminPageListItem>
+            <MainAdminPageListItem href={`${bookId}/image`}>Изображение</MainAdminPageListItem>
             <MainAdminPageListItem href={`${bookId}/people`}>Люди</MainAdminPageListItem>
             <MainAdminPageListItem href={`${bookId}/tags`}>Теги</MainAdminPageListItem>
             <MainAdminPageListItem href={`${bookId}/trans`}>Переводы</MainAdminPageListItem>
-            {/*<UpdateInfoBookForm book={book} styles={{display: 'none'}}/>*/}
         </div>
     );
 };
 
-export default UpdateBookPage;
+export default MainUpdateBookPage;
