9e39683c5f5d1f0611f61c60f1e70a10cc755301
k3ssL <viqwer3@yandex.ru>
2024-01-08 22:30:04 +0500

    написана логика удаления тега

--- src/pages/admin/books/UpdateBookTags/UI/DeleteTagListItem/DeleteTagListItem.tsx
+++ src/pages/admin/books/UpdateBookTags/UI/DeleteTagListItem/DeleteTagListItem.tsx
@@ -1,5 +1,6 @@
 import React, {FC} from 'react';
 import classes from './DeleteTagListItem.module.css'
+import {useDeleteTagListItem} from "@/pages/admin/books/UpdateBookTags/UI/DeleteTagListItem/useDeleteTagListItem";
 
 interface ITagListItemProps {
     params: {
@@ -11,11 +12,24 @@ interface ITagListItemProps {
 }
 
 const DeleteTagListItem:FC<ITagListItemProps> = ({ params: {bookId}, id, name, description}) => {
+
+    const { RemoveTagHandler, tagList} = useDeleteTagListItem(bookId)
+
+    if(!tagList) {
+        return
+    }
+
+    const handleRemoveTag = async () => {
+        const updatedTagList = tagList.filter(tag => tag.id !== id)
+
+        await RemoveTagHandler(bookId, updatedTagList)
+    }
+
     return (
         <div className={classes.container}>
             <p>{name}</p>
             <p>{description}</p>
-            <p>Удалить</p>
+            <p onClick={handleRemoveTag}>Удалить</p>
         </div>
     );
 };
