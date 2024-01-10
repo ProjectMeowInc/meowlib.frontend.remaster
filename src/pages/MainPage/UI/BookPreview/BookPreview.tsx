26b0f97021087c168698b7cf7b8519f8ed00dbba
k3ssL <viqwer3@yandex.ru>
2024-01-11 02:21:37 +0500

    добавлена иконка закладки

--- src/pages/MainPage/UI/BookPreview/BookPreview.tsx
+++ src/pages/MainPage/UI/BookPreview/BookPreview.tsx
@@ -1,6 +1,9 @@
+"use client"
+
 import React from "react"
 import classes from "./BookPreview.module.css"
 import { BASE_API_URL } from "@/app/consts"
+import { useBookPreview } from "@/pages/MainPage/UI/BookPreview/useBookPreview"
 
 interface IBookPreview {
     id: number
@@ -9,12 +12,17 @@ interface IBookPreview {
     author: string | undefined
 }
 
-const BookPreview: React.FC<IBookPreview> = ({ image, name, author }) => {
+const BookPreview: React.FC<IBookPreview> = ({ id, image, name, author }) => {
+    const { handleHover, hover } = useBookPreview(id, "InPlans")
+
     return (
         <div className={classes.container}>
             <img src={`${BASE_API_URL}/${image}`} alt={""} />
             <h4>{name}</h4>
             <p>{author}</p>
+            <div className={classes.favorite} onMouseEnter={handleHover} onMouseLeave={handleHover}>
+                {hover ? <img src={"/img/12 (2).png"} /> : <img src={"/img/11.png"} />}
+            </div>
         </div>
     )
 }
