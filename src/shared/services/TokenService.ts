d2f82101b8c6984efa3e5e072aa2512739f21920
blacksoulhub <mr.asaglaev@mail.ru>
2024-01-10 17:28:35 +0500

    create method for parsing access token

--- src/shared/services/TokenService.ts
+++ src/shared/services/TokenService.ts
@@ -1,4 +1,10 @@
 import Cookie from "js-cookie"
+import { IAccessTokenDto } from "@/entities/Auth/models/dto/AccessTokenDto"
+import { Result } from "@/shared/services/Result/Result"
+import { jwtDecode } from "jwt-decode"
+import { LogService } from "@/shared/services/LogService"
+import { IAccessTokenData } from "@/entities/Auth/models/dto/AccessTokenData"
+import { UserRoleEnum } from "@/entities/User/models/UserEntity"
 
 export class TokenService {
     static setAccessToken(accessToken: string) {
@@ -7,6 +13,22 @@ export class TokenService {
 
     static setRefreshToken(refreshToken: string) {
         Cookie.set("refresh_token", refreshToken)
+    }
+
+    static parseAccessToken(token: string): Result<IAccessTokenDto> {
+        const decodedToken = jwtDecode<IAccessTokenData>(token)
+        if (!decodedToken.exp) {
+            return Result.withError({
+                errorMessage: "У токена нет времени истечения",
+            })
+        }
+
+        const id = Number.parseInt(decodedToken.id)
+        return Result.withOk({
+            ...decodedToken,
+            id,
+            expiredTimestamp: decodedToken.exp,
+        })
     }
 
     static getAccessToken(): string | null {
