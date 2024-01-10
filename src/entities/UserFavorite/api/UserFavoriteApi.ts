02b9b58cfe152e18b99f879ce1b4c70bfb9db074
k3ssL <viqwer3@yandex.ru>
2024-01-10 23:18:45 +0500

    создан user favorite api

---
+++ src/entities/UserFavorite/api/UserFavoriteApi.ts
@@ -0,0 +1,51 @@
+import { IAddBookInFavoriteRequest } from "@/entities/UserFavorite/models/requests/IAddBookInFavoriteRequest"
+import { HTTPRequest } from "@/shared/services/HTTPResult/HTTPRequest"
+import { EmptyResult } from "@/shared/services/Result/EmptyResult"
+import { Result } from "@/shared/services/Result/Result"
+import { IGetAllBooksInFavoriteResponse } from "@/entities/UserFavorite/models/responses/IGetAllBooksInFavoriteResponse"
+import { IGetBookInFavoriteByIdResponse } from "@/entities/UserFavorite/models/responses/IGetBookInFavoriteByIdResponse"
+
+export class UserFavoriteApi {
+    static async addBookInFavorite(requestData: IAddBookInFavoriteRequest): Promise<EmptyResult> {
+        const result = await new HTTPRequest<void>()
+            .withUrl(`/v1/users/favorite`)
+            .withPostMethod()
+            .withAuth()
+            .withBody(requestData)
+            .sendAsync()
+
+        if (result.hasError()) {
+            return EmptyResult.withError(result.getError())
+        }
+
+        return EmptyResult.withOk()
+    }
+
+    static async getAllBooksInFavorite(): Promise<Result<IGetAllBooksInFavoriteResponse>> {
+        const result = await new HTTPRequest<IGetAllBooksInFavoriteResponse>()
+            .withUrl(`/v1/users/favorite/my`)
+            .withGetMethod()
+            .withAuth()
+            .sendAsync()
+
+        if (result.hasError()) {
+            return Result.withError(result.getError())
+        }
+
+        return Result.withOk(result.unwrap())
+    }
+
+    static async getBookInFavoriteById(bookId: number): Promise<Result<IGetBookInFavoriteByIdResponse>> {
+        const result = await new HTTPRequest<IGetBookInFavoriteByIdResponse>()
+            .withUrl(`/v1/ysers/favorite/my/book/${bookId}`)
+            .withGetMethod()
+            .withAuth()
+            .sendAsync()
+
+        if (result.hasError()) {
+            return Result.withError(result.getError())
+        }
+
+        return Result.withOk(result.unwrap())
+    }
+}
