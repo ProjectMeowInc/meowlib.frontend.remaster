d69814fa91d7fe611a8f15eaf3904e950419fb1b
blacksoulhub <mr.asaglaev@mail.ru>
2024-01-11 22:02:49 +0500

    update layout

--- src/shared/Layout/Layout.tsx
+++ src/shared/Layout/Layout.tsx
@@ -1,16 +1,12 @@
 import React, { PropsWithChildren } from "react"
 import Header from "@/shared/UI/Header/Header"
-import Menu from "@/pages/MainPage/UI/Menu/Menu"
 import classes from "./Layout.module.css"
 
 const Layout: React.FC<PropsWithChildren> = ({ children }) => {
     return (
         <div className={classes.container}>
             <Header />
-            <div className={classes.wrapper}>
-                <Menu />
-                {children}
-            </div>
+            {children}
         </div>
     )
 }
