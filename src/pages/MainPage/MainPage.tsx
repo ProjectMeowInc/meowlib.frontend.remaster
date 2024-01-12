d747cf7cc55ed4b050575342ea780fa49c4ebfc6
blacksoulhub <mr.asaglaev@mail.ru>
2024-01-12 16:25:04 +0500

    update main page

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,13 +1,15 @@
 import React from "react"
 import classes from "@/pages/MainPage/MainPage.module.css"
-import LastBookUpdates from "@/pages/MainPage/UI/MainPageBooks/MainPageBooks"
+import LastBookUpdates from "@/pages/MainPage/UI/LastBookUpdates/LastBookUpdates"
 import InputWithIcon from "@/shared/UI/inputWithIcon/InputWithIcon"
+import LastReviews from "@/pages/MainPage/UI/LastReview/LastReviews"
 
 const MainPage = () => {
     return (
         <div className={classes.container}>
             <InputWithIcon placeholder={"Начните вводить"} icon={"/img/10.png"} />
             <LastBookUpdates />
+            <LastReviews/>
         </div>
     )
 }
