c2f9a6c5f0a6c34219a84f26672b1490c2db7f55
blacksoulhub <mr.asaglaev@mail.ru>
2024-01-12 22:24:20 +0500

    add dots

--- src/pages/MainPage/UI/ReviewPreview/ReviewPreview.tsx
+++ src/pages/MainPage/UI/ReviewPreview/ReviewPreview.tsx
@@ -4,6 +4,8 @@ import Image from "next/image"
 import positiveIcon from "@/public/img/icons/smile-positive.svg"
 import negativeIcon from "@/public/img/icons/smile-negative.svg"
 
+const MaxPreviewReviewTextLength = 230
+
 interface IReviewPreviewProps {
     name: string
     caption: string
@@ -11,26 +13,22 @@ interface IReviewPreviewProps {
     text: string
 }
 
-const ReviewPreview: FC<IReviewPreviewProps> = ({name, caption, isPositive, text}) => {
+const ReviewPreview: FC<IReviewPreviewProps> = ({ name, caption, isPositive, text }) => {
     return (
         <div className={classes.container}>
             <div className={classes.top}>
-                {
-                    isPositive
-                    ? <Image src={positiveIcon} alt={"positive smile"}/>
-                    : <Image src={negativeIcon} alt={"negative smile"}/>
-                }
-                <div>
-                    <div className={classes.name}>
-                        {name}
-                    </div>
-                    <div className={classes.caption}>
-                        {caption}
-                    </div>
+                {isPositive ? (
+                    <Image src={positiveIcon} alt={"positive smile"} />
+                ) : (
+                    <Image src={negativeIcon} alt={"negative smile"} />
+                )}
+                <div className={classes.main_data}>
+                    <div className={classes.name}>{name}</div>
+                    <div className={classes.caption}>{caption}</div>
                 </div>
             </div>
             <div className={classes.text}>
-                {text}
+                {text.length > MaxPreviewReviewTextLength ? text.slice(0, MaxPreviewReviewTextLength) + "..." : text}
             </div>
         </div>
     )
