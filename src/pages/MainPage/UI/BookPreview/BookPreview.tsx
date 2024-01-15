0e820c7ac8149db6bb152ae3853250f252b32ab4
k3ssL <viqwer3@yandex.ru>
2024-01-15 22:15:16 +0500

    исправление

--- src/pages/MainPage/UI/BookPreview/BookPreview.tsx
+++ src/pages/MainPage/UI/BookPreview/BookPreview.tsx
@@ -3,7 +3,6 @@
 import React from "react"
 import classes from "./BookPreview.module.css"
 import { BASE_API_URL } from "@/app/consts"
-import { useBookPreview } from "@/pages/MainPage/UI/BookPreview/useBookPreview"
 
 interface IBookPreview {
     id: number
@@ -12,17 +11,13 @@ interface IBookPreview {
     author: string | undefined
 }
 
-const BookPreview: React.FC<IBookPreview> = ({ id, image, name, author }) => {
-    const { handleHover, hover } = useBookPreview(id, "InPlans")
+const BookPreview: React.FC<IBookPreview> = ({ image, name, author }) => {
 
     return (
         <div className={classes.container}>
             <img src={`${BASE_API_URL}/${image}`} alt={""} />
             <h4>{name}</h4>
             <p>{author}</p>
-            <div className={classes.favorite} onMouseEnter={handleHover} onMouseLeave={handleHover}>
-                {hover ? <img src={"/img/12.png"} alt={''} /> : <img src={"/img/11.png"} alt={''}/>}
-            </div>
         </div>
     )
 }
