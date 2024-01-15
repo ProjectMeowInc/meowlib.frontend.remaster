b257a71fa8ebc7368b68b940a590130d2302ba87
lx1cx <lagunov.2019@listy.ru>
2024-01-15 22:13:12 +0500

    Были переписаны стили кнопки

--- src/shared/UI/Header/Header.tsx
+++ src/shared/UI/Header/Header.tsx
@@ -1,15 +1,30 @@
+"use client"
+
 import React from "react"
 import classes from "./Header.module.css"
 import BurgerButton from "@/shared/UI/BurgerButton/BurgerButton"
 import Logo from "@/shared/UI/Logo/Logo"
 import HeaderItem from "@/shared/UI/HeaderItem/HeaderItem"
+import Button from "@/shared/UI/button/Button"
+import { useRouter } from "next/navigation"
 
 const Header = () => {
+
+    const router = useRouter()
+
     return (
         <div className={classes.header}>
-            <Logo />
-            <HeaderItem text={"Каталог"} href={"catalog"} />
-            <HeaderItem text={"Поиск"} href={"search"} />
+            <div className={classes.left_side}>
+                <Logo />
+                <HeaderItem text={"Каталог"} href={"catalog"} />
+                <HeaderItem text={"Поиск"} href={"search"} />
+            </div>
+
+            <div className={classes.wrapper}>
+                <Button onClick={() => router.push("/auth")}>
+                    Войти
+                </Button>
+            </div>
             <BurgerButton />
         </div>
     )
