d1cfc136d17db271c061f7e44ededaf6d8491a2c
Lx1Cx <lagunov.2019@list.ru>
2024-01-17 06:43:56 +0500

    Написан сервис для команды

---
+++ src/entities/Team/services/TeamService.ts
@@ -0,0 +1,33 @@
+import { IShortTeamDto } from "@/entities/Team/models/dto/IShortTeamDto"
+import { EmptyResult } from "@/shared/services/Result/EmptyResult"
+import { TeamApi } from "@/entities/Team/api/TeamApi"
+import { Result } from "@/shared/services/Result/Result"
+import { TeamEntity } from "@/entities/Team/TeamEntity"
+import { TeamRoleType } from "@/entities/Team/types/TeamRoleType"
+
+export class TeamService {
+    
+    static async createTeamAsync(requestData: IShortTeamDto): Promise<EmptyResult> {
+        return await TeamApi.createTeamAsync(requestData)
+    }
+    
+    static async getTeamByIdAsync(teamId: number): Promise<Result<TeamEntity>> {
+        return await TeamApi.getTeamByIdAsync(teamId)
+    }
+
+    static async updateTeamRoleAsync(teamId: number, userId: number, newRole: TeamRoleType): Promise<EmptyResult> {
+        return await TeamApi.updateTeamRoleAsync(teamId, userId, {newRole})
+    }
+
+    static async leaveTeamAsync(teamId: number): Promise<EmptyResult> {
+        return await TeamApi.leaveTeamAsync(teamId)
+    }
+
+    static async invitePeopleToTeamAsync(teamId: number, userId: number): Promise<EmptyResult> {
+        return await TeamApi.invitePeopleToTeamAsync(teamId, userId)
+    }
+
+    static async removePeopleFromTeamAsync(teamId: number, userId: number) {
+        return await TeamApi.removePeopleFromTeamAsync(teamId, userId)
+    }
+}
