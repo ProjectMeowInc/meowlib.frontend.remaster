8305d8c36e0c500dd51d181435aaaaf0bc7c1d20
BlackSoulHub <mr.asaglaev@mail.ru>
2024-01-18 07:54:49 +0500

    style fix

--- src/pages/BookPages/BookPage/UI/SelectStatusButton/useSelectStatusButton.ts
+++ src/pages/BookPages/BookPage/UI/SelectStatusButton/useSelectStatusButton.ts
@@ -3,21 +3,23 @@ import { TokenService } from "@/shared/services/TokenService"
 import { UserBookStatus } from "@/entities/UserFavorite/UserBookStatuses"
 import { UserFavoriteService } from "@/entities/UserFavorite/service/UserFavoriteService"
 import { AlertService } from "@/shared/services/AlertService"
-import { RedirectService } from "@/shared/services/RedirectService";
+import { RedirectService } from "@/shared/services/RedirectService"
 
-export const useSelectStatusButton = ( bookId: number ) => {
+export const useSelectStatusButton = (bookId: number) => {
     const [isOpen, setIsOpen] = useState<boolean>(false)
 
-    const ClickHandler = () => {
-        setIsOpen((prevState) => !prevState)
-    }
+    //todo: надо решить, какой стиль именования функций
+    // function vs const
+    // CapitalizeName vs camelCase:
+    // ClickHandler / clickHandler
+    const ClickHandler = () => setIsOpen((prevState) => !prevState)
 
-    async function ClickStatusHandler ( status: UserBookStatus ) {
+    async function ClickStatusHandler(status: UserBookStatus) {
         const result = await UserFavoriteService.addBookInFavorite(bookId, status)
 
         if (!TokenService.isLogIn()) {
-            RedirectService.redirect('/auth')
-            return AlertService.errorMessage('Вам нужно авторизоваться')
+            RedirectService.redirect("/auth")
+            return AlertService.errorMessage("Вам нужно авторизоваться")
         }
 
         if (result.hasError()) {
