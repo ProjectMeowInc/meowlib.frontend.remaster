df44e2d1cc1040a1c750a7490679ed10fcb9fb64
BlackSoulHub <mr.asaglaev@mail.ru>
2024-01-18 07:54:31 +0500

    add display pretty book statuses, fix key waring

--- src/pages/BookPages/BookPage/UI/SelectStatusButton/SelectStatusButton.tsx
+++ src/pages/BookPages/BookPage/UI/SelectStatusButton/SelectStatusButton.tsx
@@ -1,7 +1,7 @@
-'use client'
+"use client"
 
 import React, { FC } from "react"
-import { UserBookStatus, UserBookStatuses } from "@/entities/UserFavorite/UserBookStatuses"
+import { getPrettyBookStatusName, UserBookStatus, UserBookStatuses } from "@/entities/UserFavorite/UserBookStatuses"
 import classes from "./selectStatusButton.module.css"
 import { useSelectStatusButton } from "@/pages/BookPages/BookPage/UI/SelectStatusButton/useSelectStatusButton"
 import Button from "@/shared/UI/button/Button"
@@ -11,17 +11,17 @@ interface ISelectStatusButtonProps {
 }
 
 const SelectStatusButton: FC<ISelectStatusButtonProps> = ({ bookId }) => {
-    const { ClickStatusHandler, ClickHandler, isOpen  } = useSelectStatusButton(bookId)
+    const { ClickStatusHandler, ClickHandler, isOpen } = useSelectStatusButton(bookId)
 
     return (
         <div className={classes.wrapper}>
-            <Button onClick={ ClickHandler } styles={{ margin: "15px" }}>
+            <Button onClick={ClickHandler} styles={{ margin: "15px" }}>
                 Изменить статус
             </Button>
             <div className={isOpen ? classes.select_list_active : classes.select_list}>
-                {UserBookStatuses.map((status: UserBookStatus) => (
-                    <p key={""} className={classes.select_list_item} onClick={() => ClickStatusHandler(status)}>
-                        {status}
+                {UserBookStatuses.map((status: UserBookStatus, index) => (
+                    <p key={index} className={classes.select_list_item} onClick={() => ClickStatusHandler(status)}>
+                        {getPrettyBookStatusName(status)}
                     </p>
                 ))}
             </div>
