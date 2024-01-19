2bcfb83daca9277f3d1f40c99ff6c47d202a34fc
Lx1Cx <lagunov.2019@list.ru>
2024-01-19 22:47:03 +0500

    Сделана карточка команды

---
+++ src/shared/UI/TeamView/TeamView.tsx
@@ -0,0 +1,34 @@
+import { FC } from "react"
+import classes from "./teamView.module.css"
+import StatItem from "@/shared/UI/TeamView/UI/StatItem/StatItem"
+
+import people from "@/public/img/icons/user-group.png"
+import heart from "@/public/img/icons/heart.png"
+import collections from "@/public/img/icons/collection.png"
+
+interface ITeamViewProps {
+    name: string
+    description: string
+}
+
+const TeamView: FC<ITeamViewProps> = ({name, description}) => {
+    return (
+        <div className={classes.wrapper}>
+            <div className={classes.img}>
+
+            </div>
+            <div className={classes.text}>
+                <p className={classes.caption}>{name}</p>
+                <p>{description}</p>
+
+                <div className={classes.stats}>
+                    <StatItem count={0} image={people} />
+                    <StatItem count={0} image={heart} />
+                    <StatItem count={0} image={collections} />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default TeamView
