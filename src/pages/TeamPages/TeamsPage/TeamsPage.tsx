7fee65b4bf083c3f03d7591d90d425526ab17a6d
Lx1Cx <lagunov.2019@list.ru>
2024-01-19 22:45:59 +0500

    Сделана страница команд

---
+++ src/pages/TeamPages/TeamsPage/TeamsPage.tsx
@@ -0,0 +1,12 @@
+import TeamView from "@/shared/UI/TeamView/TeamView"
+
+const TeamsPage = () => {
+
+    return (
+        <div>
+            <TeamView name={"Очень длинное название"} description={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque mollis."}/>
+        </div>
+    )
+}
+
+export default TeamsPage
