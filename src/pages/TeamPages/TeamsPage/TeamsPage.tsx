8f6171fed394d6658352f8c3edcddafd754482fb
Lx1Cx <lagunov.2019@list.ru>
2024-01-21 15:44:24 +0500

    Сделана страница для вывода всех команд

--- src/pages/TeamPages/TeamsPage/TeamsPage.tsx
+++ src/pages/TeamPages/TeamsPage/TeamsPage.tsx
@@ -1,10 +1,38 @@
 import TeamView from "@/shared/UI/TeamView/TeamView"
+import { TeamService } from "@/entities/Team/services/TeamService"
+import { FC } from "react"
+import EmptyTag from "@/shared/UI/EmptyTag/EmptyTag"
+import classes from "./teamsPage.module.css"
+import Controls from "@/pages/TeamPages/TeamsPage/UI/Controls/Controls"
 
-const TeamsPage = () => {
+interface ITeamsPageProps {
+    searchParams: {
+        page: string
+    }
+}
+
+const TeamsPage: FC<ITeamsPageProps> = async ({searchParams: {page}}) => {
+
+    const getTeamsResult = await TeamService.getAllTeamsAsync(Number(page))
+
+    if (getTeamsResult.hasError()) {
+        return <EmptyTag>{getTeamsResult.getError().errorMessage}</EmptyTag>
+    }
+
+    const teams = getTeamsResult.unwrap()
 
     return (
-        <div>
-            <TeamView name={"Очень длинное название"} description={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque mollis."}/>
+        <div className={classes.wrapper}>
+            <div className={classes.teams}>
+                {teams.map(t => (
+                    <TeamView
+                        key={t.id}
+                        name={t.name}
+                        description={t.description}
+                    />
+                ))}
+            </div>
+            <Controls page={page}/>
         </div>
     )
 }
