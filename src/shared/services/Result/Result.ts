e14a190e0a22de3bc9b102d8ff17c95d6761242f
BlackSoulHub <mr.asaglaev@mail.ru>
2024-01-21 09:46:39 +0500

    move result to package

--- src/shared/services/Result/Result.ts
+++ src/shared/services/Result/Result.ts
@@ -1,39 +1,4 @@
 import { IError } from "@/shared/services/Result/IError"
+import { Result as CResult } from "result/src/Result"
 
-export class Result<TType> {
-    private readonly value: TType | null
-    private readonly error: IError | null
-
-    protected constructor(value: TType | null, error: IError | null) {
-        this.value = value
-        this.error = error
-    }
-
-    unwrap(): TType {
-        if (!this.value) {
-            throw new Error("Значение не может быть пусто")
-        }
-
-        return this.value
-    }
-
-    hasError(): boolean {
-        return this.error !== null
-    }
-
-    getError(): IError {
-        if (!this.error) {
-            throw new Error("Ошибка не может быть пуста")
-        }
-
-        return this.error
-    }
-
-    static withOk<TResult>(value: TResult): Result<TResult> {
-        return new Result<TResult>(value, null)
-    }
-
-    static withError<TResult>(error: IError): Result<TResult> {
-        return new Result<TResult>(null, error)
-    }
-}
+export class Result<TResult> extends CResult<TResult, IError> { }
