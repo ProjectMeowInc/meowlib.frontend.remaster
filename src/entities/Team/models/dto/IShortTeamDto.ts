4c20d10407a74c85c425f66a89831df0f906c734
Lx1Cx <lagunov.2019@list.ru>
2024-01-21 15:45:07 +0500

    Добавлены поля

--- src/entities/Team/models/dto/IShortTeamDto.ts
+++ src/entities/Team/models/dto/IShortTeamDto.ts
@@ -1,4 +1,5 @@
 export interface IShortTeamDto {
+    id: number
     name: string
     description: string
 }
--- src/entities/Team/models/responses/IGetAllTeamsResponse.ts
+++ src/entities/Team/models/responses/IGetAllTeamsResponse.ts
@@ -2,5 +2,6 @@ export interface IGetAllTeamsResponse {
     items: {
         id: number
         name: string
+        description: string
     }[]
 }
