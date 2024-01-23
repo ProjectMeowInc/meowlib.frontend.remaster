6c21959d170acb5e5adf6213cf5b2c54f497963d
Lx1Cx <lagunov.2019@list.ru>
2024-01-24 02:42:42 +0500

    Добавлен компонент уведомления

--- src/shared/UI/NotificationItem/NotificationItem.tsx
+++ src/shared/UI/NotificationItem/NotificationItem.tsx
@@ -1,9 +1,22 @@
 import classes from "./notificationItem.module.css"
+import { FC, ReactNode } from "react"
+import { useNotificationItem } from "@/shared/UI/NotificationItem/useNotificationItem"
+import { NotificationType } from "@/entities/Notification/types/NotificationType"
 
-const NotificationItem = () => {
+interface INotificationItemProps {
+    id: number
+    title: ReactNode
+    type: NotificationType
+    payload: string
+}
+
+const NotificationItem: FC<INotificationItemProps> = ({title, id, payload, type}) => {
+    const {getButton} = useNotificationItem(id, payload, type)
+
     return (
-        <div>
-
+        <div className={classes.wrapper}>
+            <div className={classes.title}>{title}</div>
+            {getButton()}
         </div>
     )
 }
