c856fb15446ee74caab9ee585ca5f86f9662446d
Lx1Cx <lagunov.2019@list.ru>
2024-01-24 02:40:42 +0500

    Добавлена кнопа с уведомлением

--- src/shared/UI/Header/Header.tsx
+++ src/shared/UI/Header/Header.tsx
@@ -8,11 +8,14 @@ import HeaderItem from "@/shared/UI/HeaderItem/HeaderItem"
 import Button from "@/shared/UI/button/Button"
 import { useRouter } from "next/navigation"
 import NotificationPanel from "@/shared/UI/NotificationPanel/NotificationPanel"
+import Image from "next/image"
+
+import bell from "@/public/img/icons/bell.svg"
 
 const Header = () => {
 
     const router = useRouter()
-    const [isNotificationPanelActive, setIsNotificationPanelActive] = useState<boolean>(true)
+    const [isNotificationPanelActive, setIsNotificationPanelActive] = useState<boolean>(false)
 
     return (
         <div className={classes.header}>
@@ -20,10 +23,15 @@ const Header = () => {
                 <Logo />
                 <HeaderItem text={"Каталог"} href={"catalog"} />
                 <HeaderItem text={"Поиск"} href={"search"} />
-                {isNotificationPanelActive && <NotificationPanel/>}
+                <HeaderItem text={"Команды"} href={"teams?page=1"}/>
             </div>
 
             <div className={classes.wrapper}>
+                <div onClick={() => setIsNotificationPanelActive(prevState => !prevState)} className={isNotificationPanelActive ? classes.bell_active : classes.bell}>
+                    <Image src={bell} alt={"bell"}/>
+                </div>
+                {isNotificationPanelActive && <NotificationPanel/>}
+
                 <Button onClick={() => router.push("/auth")}>
                     Войти
                 </Button>
