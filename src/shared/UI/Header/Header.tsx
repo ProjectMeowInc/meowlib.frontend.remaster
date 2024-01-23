ddf03ddd7c94ec6e290c80aad315158ee5f689d3
lx1cx <lagunov.2019@list.ru>
2024-01-23 18:46:12 +0500

    Панель добавлена в headear

--- src/shared/UI/Header/Header.tsx
+++ src/shared/UI/Header/Header.tsx
@@ -1,16 +1,18 @@
 "use client"
 
-import React from "react"
+import React, { useState } from "react"
 import classes from "./Header.module.css"
 import BurgerButton from "@/shared/UI/BurgerButton/BurgerButton"
 import Logo from "@/shared/UI/Logo/Logo"
 import HeaderItem from "@/shared/UI/HeaderItem/HeaderItem"
 import Button from "@/shared/UI/button/Button"
 import { useRouter } from "next/navigation"
+import NotificationPanel from "@/shared/UI/NotificationPanel/NotificationPanel"
 
 const Header = () => {
 
     const router = useRouter()
+    const [isNotificationPanelActive, setIsNotificationPanelActive] = useState<boolean>(true)
 
     return (
         <div className={classes.header}>
@@ -18,6 +20,7 @@ const Header = () => {
                 <Logo />
                 <HeaderItem text={"Каталог"} href={"catalog"} />
                 <HeaderItem text={"Поиск"} href={"search"} />
+                {isNotificationPanelActive && <NotificationPanel/>}
             </div>
 
             <div className={classes.wrapper}>
