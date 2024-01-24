378f34b662e6fed2d4bbaf22291a431c800ced44
k3ssL <viqwer3@yandex.ru>
2024-01-24 23:16:01 +0500

    написана логика вывода истории изменения монет, сортировка по датам, style

--- src/pages/CoinPage/UI/UpdateCoinList/UpdateCoinList.tsx
+++ src/pages/CoinPage/UI/UpdateCoinList/UpdateCoinList.tsx
@@ -1,32 +1,44 @@
 "use client"
 
 import React from "react"
-import { useUpdateCoinList } from "@/pages/CoinPage/UI/UpdateCoinList/useUpdateCoinList"
 import Preloader from "@/pages/admin/UI/Preloader/Preloader"
 import UpdateCoinListItem from "@/pages/CoinPage/UI/UpdateCoinListItem/UpdateCoinListItem"
 import classes from "./updateCoinList.module.css"
+import useUpdateCoinList from "@/pages/CoinPage/UI/UpdateCoinList/useUpdateCoinList"
 
-const UpdateCoinList = () => {
-    const { coins } = useUpdateCoinList()
+const UpdateCoinList: React.FC = () => {
+    const { groupedCoins, getMonth } = useUpdateCoinList()
 
-    if (!coins) {
+    if (!groupedCoins) {
         return <Preloader />
     }
-
     return (
         <div>
-            {coins.map((coin, index) => (
-                <div key={coin.id}>
-                    <UpdateCoinListItem
-                        id={coin.id}
-                        key={coin.id}
-                        value={coin.value}
-                        reason={coin.reason}
-                        date={coin.date}
-                    />
-                    {index < coins.length - 1 && <hr className={classes.line} />}
-                </div>
-            ))}
+
+            {Object.entries(groupedCoins)
+                .sort(([dateA], [dateB]) => new Date(dateB).getTime() - new Date(dateA).getTime())
+                .map(([date, coinsGroup]) => (
+                    <div key={date} className={classes.date_list}>
+                        <div className={classes.date}>
+                            <hr/>
+                            <div className={classes.circle}></div>
+                            <h3>{new Date(date).toLocaleDateString(undefined, { day: "numeric" })}</h3>
+                            <h3>{getMonth(date.split("-")[1])}</h3>
+                        </div>
+                        {coinsGroup.map((coin, index) => (
+                            <div key={coin.id}>
+                                <UpdateCoinListItem
+                                    id={coin.id}
+                                    value={coin.value}
+                                    reason={coin.reason}
+                                    date={coin.date}
+                                />
+                                {index < coinsGroup.length - 1 && <hr className={classes.line} />}
+                            </div>
+                        ))}
+                    </div>
+                ))}
+            <hr className={classes.run}/>
         </div>
     )
 }
--- src/pages/CoinPage/UI/UpdateCoinList/useUpdateCoinList.ts
+++ src/pages/CoinPage/UI/UpdateCoinList/useUpdateCoinList.ts
@@ -4,19 +4,87 @@ import { CoinService } from "@/entities/Coin/service/CoinService"
 import { useState } from "react"
 import { ICoinsDTO } from "@/entities/Coin/models/dto/ICoinsDTO"
 
-export const useUpdateCoinList = () => {
-    const [coins, setCoins] = useState<ICoinsDTO[]>()
+interface IGroupedCoins {
+    [key: string]: ICoinsDTO[]
+}
+
+const useUpdateCoinList = () => {
+    const [groupedCoins, setGroupedCoins] = useState<IGroupedCoins>({})
 
     useFirstLoadingAsync(async () => {
-        const getCoinsResult = await CoinService.getHistoryUpdateCoins()
-        if (getCoinsResult.hasError()) {
-            return AlertService.errorMessage(getCoinsResult.getError().errorMessage)
-        }
+            const getCoinHistoryResult = await CoinService.getHistoryUpdateCoins()
+            if (getCoinHistoryResult.hasError()) {
+                return AlertService.errorMessage(getCoinHistoryResult.getError().errorMessage)
+            }
 
-        setCoins(getCoinsResult.unwrap())
+            const sortedCoins = getCoinHistoryResult.unwrap().sort((a: ICoinsDTO, b: ICoinsDTO) => {
+                const dateA = new Date(`${a.date}`)
+                const dateB = new Date(`${b.date}`)
+                return dateB.getTime() - dateA.getTime()
+            })
+            const groupedCoinsData = sortedCoins.reduce((accumulator: IGroupedCoins, currentCoin: ICoinsDTO) => {
+                const date = currentCoin.date.split("T")[0]
+                if (!accumulator[date]) {
+                    accumulator[date] = []
+                }
+                accumulator[date].push(currentCoin)
+                return accumulator
+            }, {})
+
+            setGroupedCoins(groupedCoinsData)
     })
 
+    const getMonth = (mothNumber: string): string => {
+        let monthString
+        switch (mothNumber) {
+            case "01":
+                monthString = "января"
+                break
+            case "02":
+                monthString = "февраля"
+                break
+            case "03":
+                monthString = "марта"
+                break
+            case "04":
+                monthString = "апреля"
+                break
+            case "05":
+                monthString = "мая"
+                break
+            case "06":
+                monthString = "июня"
+                break
+            case "07":
+                monthString = "июля"
+                break
+            case "08":
+                monthString = "августа"
+                break
+            case "09":
+                monthString = "сентября"
+                break
+            case "10":
+                monthString = "октября"
+                break
+            case "11":
+                monthString = "ноября"
+                break
+            case "12":
+                monthString = "декабря"
+                break
+            default:
+                monthString = "неверный месяц"
+                break
+        }
+
+        return monthString || "неверный месяц"
+    }
+
     return {
-        coins,
+        groupedCoins,
+        getMonth,
     }
 }
+
+export default useUpdateCoinList
