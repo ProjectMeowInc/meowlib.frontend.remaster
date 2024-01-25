63d573068b9327a44546dfe5107b55d3a2c2e0a0
Lx1Cx <lagunov.2019@list.ru>
2024-01-26 02:54:06 +0500

    Сделана metadata

--- src/app/(withMenu)/teams/[teamId]/page.tsx
+++ src/app/(withMenu)/teams/[teamId]/page.tsx
@@ -1,3 +1,30 @@
 import TeamPage from "@/pages/TeamPages/TeamPage/TeamPage"
+import { Metadata } from "next"
+import { TeamService } from "@/entities/Team/services/TeamService"
+
+interface ITeamPageProps {
+    params: {
+        teamId: number
+    }
+}
+
+export const generateMetadata = async ({params: {teamId}}: ITeamPageProps): Promise<Metadata> => {
+    const getTeamsResult = await TeamService.getTeamByIdAsync(teamId)
+
+    if (getTeamsResult.hasError()) {
+        return {
+            title: "MeowLib",
+            description: "The best library of ranobe in the world",
+        }
+    }
+
+    const team = getTeamsResult.unwrap()
+
+    return {
+        title: `Команда ${team.name}`,
+        description: team.description,
+        keywords: team.name
+    }
+}
 
 export default TeamPage
