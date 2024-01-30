3680d63f9811ba890573d8487a6abb3688390e5b
Lx1Cx <lagunov.2019@list.ru>
2024-01-31 03:58:41 +0500

    warnings

--- src/pages/TeamPages/TeamPage/UI/UserModal/useAddUserModal.ts
+++ src/pages/TeamPages/TeamPage/UI/UserModal/useAddUserModal.ts
@@ -1,15 +1,13 @@
-import { useEffect, useState } from "react"
+import { useState } from "react"
 import { useFirstLoadingAsync } from "@/shared/hooks/useFirstLoadingAsync"
 import { UsersService } from "@/entities/User/service/UsersService"
 import { IUserDTO } from "@/entities/User/models/dto/IUserDTO"
 import { AlertService } from "@/shared/services/AlertService"
 import { useParams } from "next/navigation"
-import { TeamService } from "@/entities/Team/services/TeamService"
 
 export const useAddUserModal = () => {
 
     const [users, setUsers] = useState<IUserDTO[] | null>(null)
-    const params = useParams<{teamId: string}>()
 
     useFirstLoadingAsync( async () => {
         const getUsersResult = await UsersService.getAllUsers()
@@ -20,8 +18,6 @@ export const useAddUserModal = () => {
 
         setUsers(getUsersResult.unwrap())
     })
-
-
 
     return {
         users
