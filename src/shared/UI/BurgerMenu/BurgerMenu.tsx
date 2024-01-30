86beef054e8b34344b6ea0ced1a0fdfd8efbddaf
Lx1Cx <lagunov.2019@list.ru>
2024-01-31 03:53:17 +0500

    Рефакторинг

--- src/shared/UI/BurgerMenu/BurgerMenu.tsx
+++ src/shared/UI/BurgerMenu/BurgerMenu.tsx
@@ -7,7 +7,6 @@ import arrowLeft from "@/public/img/icons/arrow-left.svg"
 import Image from "next/image"
 import { useBurgerMenu } from "@/shared/UI/BurgerMenu/useBurgerMenu"
 import Button from "@/shared/UI/button/Button"
-import { router } from "next/client"
 
 interface IBurgerMenuProps {
     onClose: () => void
@@ -15,41 +14,37 @@ interface IBurgerMenuProps {
 }
 
 const BurgerMenu: FC<IBurgerMenuProps> = ({ onClose, isActive }) => {
-    const {isDisplayAdminRoutes, isAuth} = useBurgerMenu()
+    const {isDisplayAdminRoutes, isAuth, router} = useBurgerMenu()
 
     const containerClassName = `${classes.container} ` + (isActive ? classes.active : "")
 
     return (
         <div className={containerClassName}>
             <div className={classes.menu_header} onClick={onClose}>
-                <Image src={arrowLeft} alt={"arrow"} height={35} />
+                <Image src={arrowLeft} alt={"arrow"} width={35} height={35} />
                 <span className={classes.header_text}>Закрыть меню</span>
             </div>
             <div className={classes.menu_items}>
-                {
-                    isAuth ? (
-                        <div className={classes.items_group}>
-                            <div className={classes.group_name}>Аккаунт</div>
+                {isAuth
+                        ? <div className={classes.items_group}>
+                            <p className={classes.group_name}>Аккаунт</p>
                             <div className={classes.group_items}>
                                 <BurgerMenuItem href={"/my"} text={"Профиль"} imageHref={"NONE"} />
                                 <BurgerMenuItem href={"/my/settings"} text={"Другое"} imageHref={"NONE"} />
                             </div>
                         </div>
-                    ) : (
-                        <Button styles={{width: "50%"}} onClick={() => router.push("/auth")}>
+                        : <Button styles={{width: "50%"}} onClick={() => router.push("/auth")}>
                             Войти
                         </Button>
-                    )
                 }
-                {
-                    isAuth && isDisplayAdminRoutes && (
+
+                {isDisplayAdminRoutes &&
                         <div className={classes.items_group}>
-                            <div className={classes.group_name}>Админ-панель</div>
+                            <p className={classes.group_name}>Админ-панель</p>
                             <div className={classes.group_items}>
                                 <BurgerMenuItem href={"/admin"} text={"Админ-панель"} imageHref={"NONE"} />
                             </div>
                         </div>
-                    )
                 }
             </div>
         </div>
