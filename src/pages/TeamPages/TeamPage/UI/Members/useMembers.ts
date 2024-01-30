2a65598e1359d3963820d9754c3c2724c92f3297
Lx1Cx <lagunov.2019@list.ru>
2024-01-31 03:57:23 +0500

    Написан зук для пользователей

---
+++ src/pages/TeamPages/TeamPage/UI/Members/useMembers.ts
@@ -0,0 +1,44 @@
+import { TeamRoleType } from "@/entities/Team/types/TeamRoleType"
+import { useState } from "react"
+import { TokenService } from "@/shared/services/TokenService"
+import { useFirstLoading } from "@/shared/hooks/useFirstLoading"
+
+interface IUseMembersProps {
+    id: number
+    login: string
+    role: TeamRoleType
+}
+
+export const useMembers = (members: IUseMembersProps[]) => {
+
+    const [userIsAdmin, setUserIsAdmin] = useState<boolean>(false)
+    const [modalIsActive, setModalIsActive] = useState<boolean>(false)
+
+    useFirstLoading(() => {
+        const userToken = TokenService.getAccessToken()
+
+        if (!userToken) {
+            return
+        }
+
+        const parsedToken = TokenService.parseAccessToken(userToken)
+
+        if (parsedToken.hasError()) {
+            return
+        }
+
+        const user = parsedToken.unwrap()
+
+        members.find(u => {
+            if (u.id === user.id && u.role === "Admin") {
+                setUserIsAdmin(true)
+            }
+        })
+    })
+
+    return {
+        userIsAdmin,
+        modalIsActive,
+        setModalIsActive
+    }
+}
