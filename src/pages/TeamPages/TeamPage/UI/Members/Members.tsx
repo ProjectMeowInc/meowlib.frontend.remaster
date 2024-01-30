58cebd34109a2def5185d07daa2bd2264e58cf77
Lx1Cx <lagunov.2019@list.ru>
2024-01-31 03:56:46 +0500

    Рефакторинг

--- src/pages/TeamPages/TeamPage/UI/Members/Members.tsx
+++ src/pages/TeamPages/TeamPage/UI/Members/Members.tsx
@@ -1,7 +1,12 @@
+"use client"
+
 import React, { FC } from "react"
 import { TeamRoleType } from "@/entities/Team/types/TeamRoleType"
 import classes from "./members.module.css"
 import Member from "@/pages/TeamPages/TeamPage/UI/Members/UI/Member/Member"
+import { useMembers } from "@/pages/TeamPages/TeamPage/UI/Members/useMembers"
+import AddMember from "@/pages/TeamPages/TeamPage/UI/Members/UI/AddMember/AddMember"
+import UserModal from "@/pages/TeamPages/TeamPage/UI/UserModal/UserModal"
 
 interface IMembersProps {
     members: {
@@ -12,6 +17,9 @@ interface IMembersProps {
 }
 
 const Members: FC<IMembersProps> = ({members}) => {
+
+    const {userIsAdmin, modalIsActive, setModalIsActive} = useMembers(members)
+
     return (
         <div className={classes.wrapper}>
             <h1 className={classes.caption}>Участники</h1>
@@ -21,8 +29,11 @@ const Members: FC<IMembersProps> = ({members}) => {
                     id={m.id}
                     login={m.login}
                     role={m.role}
+                    userIsAdmin={true}
                 />
             ))}
+            {userIsAdmin && <AddMember onClick={() => setModalIsActive(true)}/>}
+            {modalIsActive && <UserModal onClick={() => setModalIsActive(false)}/>}
         </div>
     )
 }
--- src/pages/TeamPages/TeamPage/UI/Members/UI/Member/Member.tsx
+++ src/pages/TeamPages/TeamPage/UI/Members/UI/Member/Member.tsx
@@ -1,22 +1,35 @@
+"use client"
+
 import classes from "./member.module.css"
 import { TeamRoleType } from "@/entities/Team/types/TeamRoleType"
 import React, { FC } from "react"
+import Image from "next/image"
+
+import cross from "@/public/img/icons/cross.png"
+import { useMember } from "@/pages/TeamPages/TeamPage/UI/Members/UI/Member/useMember"
 
 interface IMemberProps {
     id: number
     login: string
     role: TeamRoleType
+    userIsAdmin: boolean
 }
 
 const Member: FC<IMemberProps> = ({id, login, role}) => {
+
+    const {DeleteHandler} = useMember()
+
     return (
         <div className={classes.wrapper}>
             <div className={classes.img}>
 
             </div>
-            <div>
-                <p className={classes.login}>{login}</p>
-                <p className={classes.role}>{role}</p>
+            <div className={classes.info}>
+                <div>
+                    <p className={classes.login}>{login}</p>
+                    <p className={classes.role}>{role}</p>
+                </div>
+                <Image src={cross} alt={"cross icon"} width={40} height={40} onClick={async () => DeleteHandler(id)}/>
             </div>
         </div>
     )
