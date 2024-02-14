4fb94f85e5cee8b76ed7dde5dea9668722bf0212
Lx1Cx <lagunov.2019@list.ru>
2024-02-15 01:40:10 +0500

    Правка стилей

--- src/pages/TeamPages/TeamPage/UI/BookList/BooksList.tsx
+++ src/pages/TeamPages/TeamPage/UI/BookList/BooksList.tsx
@@ -1,11 +1,18 @@
 import classes from "./bookList.module.css"
 import BookPreview from "@/pages/MainPage/UI/BookPreview/BookPreview"
+import { FC } from "react"
+import AddTranslateButton from "@/pages/TeamPages/TeamPage/UI/BookList/UI/AddTranslateButton/AddTranslateButton"
 
-const BooksList = () => {
+interface IBookListProps {
+    teamId: number
+}
+
+const BooksList: FC<IBookListProps> = ({ teamId }) => {
     return (
         <div className={classes.wrapper}>
+            <AddTranslateButton teamId={teamId} />
             {new Array(8).fill(0).map(() => (
-                <BookPreview key={1} id={1} image={null} name={"Some Name"}/>
+                <BookPreview key={1} id={1} image={null} name={"Some Name"} />
             ))}
         </div>
     )
